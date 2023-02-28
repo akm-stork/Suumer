74ecf26e7fa5460e302c2be8cc00e6b6dc2955d4
Ali Khalid <ali.kmassri@gmail.com>
2023-03-01 01:07:27 +0200

    dummie

--- main.js
+++ main.js
@@ -33,12 +33,13 @@ document.getElementById('prev').onclick = function () {
 
 // Dashboard 
 let tf = false;
+let oldItem = document.querySelector('.item');
 let addBtn = document.querySelector('.add-btn');
-let removeBtn = document.querySelector('.remove-btn')
 let addSec = document.querySelector('.add-element');
 let secAbout = document.getElementById('section-about');
 let cont = document.getElementById('slide');
 let inputSec = document.querySelector('.input-sec-element');
+let inputSec2 = document.querySelector('.input-sec-element2');
 addBtn.addEventListener('click', () => {
   if (tf === false) {
     tf = true;
@@ -48,6 +49,19 @@ addBtn.addEventListener('click', () => {
     tf = false;
     inputSec.classList.remove('design');
     inputSec.classList.add('display-none');
+  }
+});
+
+let removeBtn = document.querySelector('.remove-btn');
+removeBtn.addEventListener('click', () => {
+  if (tf === false) {
+    tf = true;
+    inputSec2.classList.add('design');
+    inputSec2.classList.remove('display-none');
+  } else if (tf === true) {
+    tf = false;
+    inputSec2.classList.remove('design');
+    inputSec2.classList.add('display-none');
   }
 });
 const newImg = document.querySelector('.img-in');
@@ -91,7 +105,53 @@ function addItem() {
   // console.log('hi');
 
 }
+let removableItems = `
+<h3 style="cursor: pointer; margin: 1rem;" class="btn1">${name}</h3>
+`;
+inputSec2.insertAdjacentHTML('beforeend', removableItems);
 let submitBtn = document.getElementById('create');
 submitBtn.addEventListener('click', () => {
   addItem();
 })
+let ft = false
+let item1 = document.querySelector('.item1');
+let btn1 = document.querySelector('.btn1');
+btn1.addEventListener('click', () => {
+  cont.removeChild(item1);
+  console.log('first child removed');
+})
+let item2 = document.querySelector('.item2');
+let btn2 = document.querySelector('.btn2');
+btn2.addEventListener('click', () => {
+  cont.removeChild(item2);
+  console.log('second child removed');
+})
+let item3 = document.querySelector('.item3');
+let btn3 = document.querySelector('.btn3');
+btn3.addEventListener('click', () => {
+  cont.removeChild(item3);
+  console.log('third child removed');
+})
+let item4 = document.querySelector('.item4');
+let btn4 = document.querySelector('.btn4');
+btn4.addEventListener('click', () => {
+  cont.removeChild(item4);
+  console.log('fourth child removed');
+})
+let item5 = document.querySelector('.item5');
+let btn5 = document.querySelector('.btn5');
+btn5.addEventListener('click', () => {
+  cont.removeChild(item5);
+  console.log('fifth child removed');
+})
+let item6 = document.querySelector('.item6');
+let btn6 = document.querySelector('.btn6');
+btn6.addEventListener('click', () => {
+  cont.removeChild(item6);
+  console.log('sixth child removed');
+})
+// let removeOffers = document.querySelector('.remove-sec-offers');
+// removeBtn.addEventListener("click", () => {
+//   cont.removeChild(oldItem);
+//   console.log('child removed')
+// })
