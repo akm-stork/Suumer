bce371dfec6a611c0eb5624fb41a7269fc1fe781
Ali Khalid <ali.kmassri@gmail.com>
2023-03-12 13:24:16 +0200

    commited

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 157) {
+  if (window.scrollY > 107) {
     navbar.classList.add('new-nav');
   } else {
     navbar.classList.remove('new-nav');
